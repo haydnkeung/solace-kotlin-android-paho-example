cd7b9cca03c39cda960c30f69ba2c00f999ba001
haydnkeung <haydnkeung@gmail.com>
2020-02-08 14:16:34 -0500

    Created login page

---
+++ app/src/main/java/com/example/myapplication/Welcome.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+
+        final Button button = (Button) findViewById(R.id.buttonGo);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+}
+
